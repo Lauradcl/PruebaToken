8e78870b24d3f923ea504337268507767f892a2e
RobertoFS <roberto.fernandez48@educa.madrid.org>
2025-02-11 13:09:31 +0100

    3 modificacion

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -15,6 +15,7 @@ public class Prueba {
      */
     public static void main(String[] args) {
         System.out.println("Hola muy buenas ");
+        System.out.println("El mio es el que manda ");
         
     }
     
